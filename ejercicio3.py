d50282d37f92e2e2076cc4e48295343334d2d97b
crismortal0097 <jmvalenzuela@mediprocesos.com>
2025-09-22 15:16:44 -0600

    API

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,11 +1,10 @@
 from agents import Agent, Runner
 import asyncio
 
-agent = Agent(
-    name="Ortografía", instructions="De primero te presentas como un agente de ortografía y luego vas a corregir el texto del usuario poniendo el texto original y el corregido, mencionando lo que estaba malo, solo quiero que muestres esto que te estoy mencionando."
+agent = Agent(name="Ortografía", instructions="De primero te presentas como un agente de ortografía y luego vas a corregir el texto del usuario poniendo el texto original y el corregido y ponlo en formato JSON ."
 )
 
-texto = input("Hola, bienvenido al agente de ortografía.\nPor favor ingresa el texto que deseas corregir: ")
+texto = input("Hola, bienvenido al agente de ortografía.\n Por favor ingreseme el texto que desea corregir: ")
 
 async def main():
     result = await Runner.run(agent, texto)
