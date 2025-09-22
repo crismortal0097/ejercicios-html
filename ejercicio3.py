910a60b5e2e9e835052fce805e38169299d28acb
crismortal0097 <jmvalenzuela@mediprocesos.com>
2025-09-22 08:36:34 -0600

    nuevo ejercicio

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,10 +1,7 @@
 from agents import Agent, Runner
 
-agent = Agent(name="Assistant", instructions="You are a helpful assistant")
+agent = Agent(name="Ortografia", instructions="De primero te presentas como un agente de ortografia y luego, tu tarea es ayudar al usuario a corregir sus textos y poner que estaba mal escrito y identificar si lleva algun tipo de signo si llega a ingresar algun tipo de dato numerico le diras que no se puede corregir este dato y si te dan las gracias a ti solo agradece de regreso diciendo que tenga un buen dia.")
 
-result = Runner.run_sync(agent, "Write a haiku about recursion in programming.")
+result = Runner.run_sync(agent, "Gracias.")
 print(result.final_output)
 
-# Code within the code,
-# Functions calling themselves,
-# Infinite loop's.
